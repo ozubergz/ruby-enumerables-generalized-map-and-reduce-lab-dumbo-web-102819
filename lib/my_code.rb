baf6fccb118a7e78f140bf4a75eafc8b83e8f5f8
James Min <jmin499@gmail.com>
2019-10-09 01:38:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,5 +11,17 @@ end
 
 def reduce(array, start_value = nil)
   if start_value
+    accum = start_value
+    i = 0
+  else
+    accum = array[0]
+    i = 1
+  end
+  
+  while i < array.length do
+    accum = yield(accum, array[i])
+    i += 1
+  end
+  accum
 end
 
