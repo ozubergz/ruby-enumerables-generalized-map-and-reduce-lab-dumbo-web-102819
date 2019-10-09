c6794cf197ed8611b0dbf05b77439437abc27dfd
James Min <jmin499@gmail.com>
2019-10-09 01:14:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,14 +10,6 @@ def map(array)
 end
 
 def reduce(array, start_value = 0)
-  new = []
-  i = 0
-  while i < array.length do
-    init_val = 0
-    init_val += array[i]
-    yield(init_val)
-    i = i + 1
-  end
-  new
+  
 end
 
