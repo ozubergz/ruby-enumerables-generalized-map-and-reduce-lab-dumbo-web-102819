457301db0a6ce6e71805176930b6f124064ac823
James Min <jmin499@gmail.com>
2019-10-09 01:14:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
-# Your Code Here
+
+def map(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new.push(yield(array[i]))
+    i = i + 1
+  end
+  new
+end
+
+def reduce(array, start_value = 0)
+  new = []
+  i = 0
+  while i < array.length do
+    init_val = 0
+    init_val += array[i]
+    yield(init_val)
+    i = i + 1
+  end
+  new
+end
+
